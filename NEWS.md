0b841c5bad9ae3b2c39255f282a11ae57e9c49a4
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2019-04-08 16:43:15 -0700

    Initial commit.

---
+++ NEWS.md
@@ -0,0 +1,2 @@
+# sharkbox 0.1.0
+First release
---
+++ R/aaa-.R
@@ -0,0 +1,4 @@
+#' @importFrom stats optim pnorm rexp rnorm rpois runif weighted.mean
+NULL
+#' @importFrom utils as.relistable read.csv relist write.csv
+NULL
