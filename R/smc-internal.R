e5126ba8c206359295150e38f5957e61db063a57
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2019-08-21 17:04:10 -0700

    smc model - step 1

--- R/smc-internal.R
+++ R/smc-internal.R
@@ -106,7 +106,6 @@ newSMC = function(G, S, L=3) {
   return(x0)
 }
 
-
 .removeBumps = function(x, thr) {
 
   x = rle(x)
@@ -147,9 +146,9 @@ newSMC = function(G, S, L=3) {
   return(x)
 }
 
-.getTM = function(x, S=NULL, simplify=FALSE) {
-  if(is.null(S)) S = max(x)
-  x = factor(x, levels=seq_len(S))
+.getSimpleTM = function(x, levels, simplify=FALSE, diagonal2zero=FALSE) {
+  x = as.factor(x)
+  levels(x) = levels
   out = table(head(x, -1), tail(x, -1))
   if(isTRUE(simplify)) {
     cs = colSums(out)
@@ -160,28 +159,17 @@ newSMC = function(G, S, L=3) {
       out = out[, -ind]
     }
   }
+  if(isTRUE(diagonal2zero)) diag(out) = 0
   return(out)
 }
 
-.countTraj = function(x, S, L=3, thr=0.03) {
-  # x has two columns: species and size-class
-  if(ncol(x)>2) {
-    y = x[, 1]
-    x = x[, 2:3]
-  } else {
-    y = findLandingGroups(x[, 1], S=S, thr=thr)
-  }
-  x0 = tapply(x[,1], INDEX = y, FUN = .getTM, S=S)
-  x1 = tapply(x[,2], INDEX = x[,1], FUN = .getTM, S=L)
-  mainSp = apply(table(y, factor(x[,1], levels=seq_len(S))), 1, which.max)
-  nn = newSMC(G=S, S=S, L=L)
-  nn$species[mainSp] = x0
-  nn$groups$prop[mainSp] = table(y)
-  nn$groups$jump[mainSp, mainSp] = .getTM(x=y, simplify=TRUE)
-  diag(nn$groups$jump) = 0
-  nn$size[as.numeric(names(x1))] = x1
-  return(nn)
+estimateTM = function(x, INDEX, ...) {
+  x0 = tapply(x, INDEX = INDEX, FUN = .getSimpleTM, levels=levels(x), ...)
+  out = 0
+  for(i in seq_along(x0)) out = out + x0[[i]]
+  return(out)
 }
+
 
 .norm = function(x) {
   s = sum(x, na.rm=TRUE)
